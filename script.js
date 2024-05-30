2ee2dd79a2b911f685707356e056966be6a9cb43
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2024-05-30 12:51:26 +0530

    Problem no 1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Given an array of numbers, use the map function with an arrow function to square each element of the array.
+
+// let arr = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10];
+
+// let newArr = arr.map((number) => {
+//     return number * number;
+// })
+
+// console.log(newArr);
+
+
+
+
