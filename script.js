7b9577d17538fd996e3663d08914ea8356bf4e07
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2024-05-31 13:34:24 +0530

    Problem no 5

--- script.js
+++ script.js
@@ -55,21 +55,46 @@
 // 4. Given an array of numbers, use the filter function to create a new array containing only the prime numbers.
 
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
-let count = 0;
-let primeNumbers = arr.filter((num) => {
-    for(let i = 2; i<num ; i++){
-        if(num % i === 0){
-            return false;
-        }
-    }
-    if(num == 1){
-        return false;
-    }
-    return true;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+// let count = 0;
+// let primeNumbers = arr.filter((num) => {
+//     for(let i = 2; i<num ; i++){
+//         if(num % i === 0){
+//             return false;
+//         }
+//     }
+//     if(num == 1){
+//         return false;
+//     }
+//     return true;
 
+// })
+
+// console.log(primeNumbers);
+
+
+// 5. State different use cases of map, filter and reduce functions.
+
+
+let arr = [10, 24, 37, 40, 50, 60, 75, 80, 97, 100];
+// Map Function
+let mapArray = arr.map((num) => {
+    return num + num;
 })
+console.log("Using map.....");
+console.log(mapArray);
 
-console.log(primeNumbers);
+//filter function
+let filterArray = arr.filter((num) => {
+    return num % 10 == 0;
+})
+console.log("Using filter.....");
+console.log(filterArray);
 
+//reduce function
+let reduceNumber = arr.reduce((prev , aft) => {
+    return prev + aft;
+})
+console.log("Using reduce.....");
+console.log(reduceNumber);
 
